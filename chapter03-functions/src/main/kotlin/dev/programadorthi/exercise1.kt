f6398760a55750c4160bfd66cf928e8b0008fdea
Thiago Santos <programadorthi@gmail.com>
2021-12-14 08:47:01 -0300

    Exercise 1 added

---
+++ chapter03-functions/src/main/kotlin/dev/programadorthi/exercise1.kt
@@ -0,0 +1,10 @@
+package dev.programadorthi
+
+fun square(n: Int) = n * n
+
+fun triple(n: Int) = n * 3
+
+fun compose(
+    f: (Int) -> Int,
+    g: (Int) -> Int
+): (Int) -> Int = TODO("Implement your solution")
