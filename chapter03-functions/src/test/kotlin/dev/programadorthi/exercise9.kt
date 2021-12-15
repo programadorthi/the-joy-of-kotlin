b5cec8d19797d240a8e4b8823b0ce8d75fe218a2
Thiago Santos <programadorthi@gmail.com>
2021-12-14 23:41:20 -0300

    Exercise 9 test added

---
+++ chapter03-functions/src/test/kotlin/dev/programadorthi/exercise9.kt
@@ -0,0 +1,21 @@
+package dev.programadorthi
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.string.shouldBeEqualIgnoringCase
+import kotlin.random.Random
+import kotlin.random.nextULong
+
+class Exercise9Test : FunSpec({
+    val random = Random.Default
+
+    repeat(6) {
+        test("curried $it") {
+            val a = random.nextInt()
+            val b = random.nextDouble(until = Double.MAX_VALUE)
+            val c = random.nextULong().toString()
+            val d = random.nextBoolean()
+            val result = func(a, b, c, d)
+            result shouldBeEqualIgnoringCase "$a, $b, $c, $d"
+        }
+    }
+})
