968698ba7502fe57e871833233d5d706f138259d
Thiago Santos <programadorthi@gmail.com>
2021-12-17 23:45:09 -0300

    exercise 3 solution added

--- chapter04-recursion/src/main/kotlin/dev/programadorthi/exercise3.kt
+++ chapter04-recursion/src/main/kotlin/dev/programadorthi/exercise3.kt
@@ -2,4 +2,15 @@ package dev.programadorthi
 
 import java.math.BigInteger
 
-fun fib(x: Int): BigInteger = TODO("Implement your version")
+fun fib(x: Int): BigInteger {
+    tailrec fun fib(
+        a: BigInteger,
+        b: BigInteger,
+        c: BigInteger
+    ): BigInteger = when (c) {
+        BigInteger.ZERO -> BigInteger.ONE
+        BigInteger.ONE -> a + b
+        else -> fib(b, a + b, c - BigInteger.ONE)
+    }
+    return fib(BigInteger.ZERO, BigInteger.ONE, BigInteger.valueOf(x.toLong()))
+}
