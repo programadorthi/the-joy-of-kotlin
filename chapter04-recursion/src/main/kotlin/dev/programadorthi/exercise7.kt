69268a0e095a01e650e7f45f79ba23d2ecb3351e
Thiago Santos <programadorthi@gmail.com>
2021-12-18 20:34:26 -0300

    Exercise 7 solution added

--- chapter04-recursion/src/main/kotlin/dev/programadorthi/exercise7.kt
+++ chapter04-recursion/src/main/kotlin/dev/programadorthi/exercise7.kt
@@ -1,4 +1,7 @@
 package dev.programadorthi
 
+fun <T> prepend(list: List<T>, elem: T): List<T> =
+    listOf(elem) + list
+
 fun <T> reverse(list: List<T>): List<T> =
-    TODO("Implement using fold")
+    foldLeft(list, listOf(), ::prepend)
