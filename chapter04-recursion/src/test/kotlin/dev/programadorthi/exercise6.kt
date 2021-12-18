3ed904b6f70906cc4d447a6fa6073469b5596567
Thiago Santos <programadorthi@gmail.com>
2021-12-18 19:59:58 -0300

    Exercise 6 test added

--- chapter04-recursion/src/test/kotlin/dev/programadorthi/exercise6.kt
+++ chapter04-recursion/src/test/kotlin/dev/programadorthi/exercise6.kt
@@ -4,14 +4,14 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.string.shouldBeEqualIgnoringCase
 
-class Exercise5Test : FunSpec({
+class Exercise6Test : FunSpec({
     repeat(6) { n ->
-        test("string foldLeft $n") {
+        test("string foldRight $n") {
             val list = Array(n) { n.toChar() }.toList()
             string(list) shouldBeEqualIgnoringCase list.fold("") { s, c -> s + c }
         }
 
-        test("sum foldLeft $n") {
+        test("sum foldRight $n") {
             val list = Array(n) { it + n }.toList()
             sum(list) shouldBeExactly list.fold(0) { s, c -> s + c }
         }
